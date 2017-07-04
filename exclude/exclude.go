ab57f166ecb27647e00d21a89ecb3ae0ea71ac17
Jonathan Zacsh <jzacsh@gmail.com>
2017-07-03 20:10:37 -0400

    implements exclude.ParseHomeConf(): only partly tested
    should be safe to use package-relative paths, as i am, per:
      https://dave.cheney.net/2016/05/10/test-fixtures-in-go

--- exclude/exclude.go
+++ exclude/exclude.go
@@ -1,9 +1,38 @@
-package main
+package exclude
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func Build(system []string, homes ...[]string) ([]string, error) {
 	panic("Build() not yet implemented")
 }
 
+// Read exclude paths for a given user's home directory
+//
+// homePath: is a string indicatging path to single user's home "/home/bob" or
+// "/usrhomes/disk2/alic/" is as follows:
+// - ascii encoded text
+// - relative paths to $HOME; leading slashes will be ignored
+//
+// Return is a list of absolute paths that should be excluded
 func ParseHomeConf(homePath string) ([]string, error) {
-	panic("ParseHomeConf() not yet implemented")
+	confPath := filepath.Join(homePath, ".config/sysrestic.exclude")
+	conf, err := os.Open(confPath)
+	if err != nil {
+		return nil, err
+	}
+	var excludes []string
+
+	scanner := bufio.NewScanner(conf)
+	for scanner.Scan() {
+		excludes = append(excludes, filepath.Join(homePath, scanner.Text()))
+	}
+	if err := scanner.Err(); err != nil {
+		return excludes, fmt.Errorf("reading %s: %s", confPath, err)
+	}
+	return excludes, nil
 }
--- exclude/exclude_test.go
+++ exclude/exclude_test.go
@@ -1,11 +1,45 @@
-package main
+package exclude
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
 
+func getFixtureDir() (string, error) {
+	path := "../testdata"
+	if f, err := os.Stat(path); (f != nil && !f.IsDir()) || err != nil {
+		if f != nil && !f.IsDir() {
+			return "", fmt.Errorf(
+				"testdata was not a directory: %v", err)
+		} else {
+			return "", err
+		}
+	}
+	return path, nil
+
+}
 func TestBuild(t *testing.T) {
-	t.Errorf("tests of Buidl() not yet implemented")
+	t.Errorf("tests of Build() not yet implemented")
 }
 
-func TestParseHomeConfs(t *testing.T) {
+func TestParseHomeConf(t *testing.T) {
+	fixtureDir, err := getFixtureDir()
+	if err != nil {
+		t.Fatalf("finding testdata: %v", err)
+	}
+
+	if _, err := ParseHomeConf("/dev/null"); err == nil {
+		t.Errorf("seem OK with /dev/null")
+	}
+
+	aliceHome := filepath.Join(fixtureDir, "/home/alice")
+	exc, err := ParseHomeConf(aliceHome)
+	if err != nil {
+		t.Errorf("unexpected problem with fixture, 'alice': %v", err)
+	}
+
+	t.Logf("%d exclude paths\n", len(exc)) // TODO finish test
 	t.Errorf("tests of ParseHomeConf() not yet implemented")
 }
