cc96b25dd84d40b329fb2f3ff4f094816e473bdb
Jonathan Zacsh <jzacsh@gmail.com>
2017-07-07 22:52:39 -0400

    doc: fix explanation of *what* bulleted-list refers to

--- exclude/exclude.go
+++ exclude/exclude.go
@@ -14,9 +14,11 @@ func Build(system []string, homes ...[]string) ([]string, error) {
 // Read exclude paths for a given user's home directory
 //
 // homePath: is a string indicatging path to single user's home "/home/bob" or
-// "/usrhomes/disk2/alic/" is as follows:
+// "/usrhomes/disk2/alic/". The exclude file found under homePath will be read
+// as follows:
 // - ascii encoded text
-// - relative paths to $HOME; leading slashes will be ignored
+// - each line indicates a path to be excluded
+// - all lines are read as relative to $HOME; leading slashes ignored
 //
 // Return is a list of absolute paths that should be excluded
 func ParseHomeConf(homePath string) ([]string, error) {
