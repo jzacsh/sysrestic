762dbb5f9054cea17875215037b385d71ea194ea
Jonathan Zacsh <jzacsh@gmail.com>
2017-07-09 13:14:02 -0400

    utilize newly shared testdata/ assertion of be03743a185e3

--- usr/list_test.go
+++ usr/list_test.go
@@ -1,11 +1,19 @@
 package usr
 
-import "testing"
+import (
+	"testing"
+
+	"../test"
+)
+
+const fixtureDir string = "../testdata"
 
 func TestLoadPasswdFrom(t *testing.T) {
+	test.AssertFixtureDir(t, fixtureDir)
 	t.Fatalf("LoadPasswdFrom() not yet tested")
 }
 
 func TestListUsers(t *testing.T) {
+	test.AssertFixtureDir(t, fixtureDir)
 	t.Fatalf("ListUsers() not yet tested")
 }
