400a7c18a571674809cf0f5237524133ea20dc01
Jonathan Zacsh <jzacsh@gmail.com>
2017-07-09 11:23:22 -0400

    nop(readability): mv test to expected place for cli.go per 3548ea3e083dfb5

---
+++ cli_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func Test_parseCli(t *testing.T) {
+	t.Errorf("Test of parseCli() not yet implemented")
+}
--- main_test.go
+++ main_test.go
@@ -2,10 +2,6 @@ package main
 
 import "testing"
 
-func Test_parseCli(t *testing.T) {
-	t.Errorf("Test of parseCli() not yet implemented")
-}
-
 func Test_parseExcludes(t *testing.T) {
 	t.Errorf("Test of parseExcludes() not yet implemented")
 }
