3548ea3e083dfb5656f5b669bebae78fc66cc675
Jonathan Zacsh <jzacsh@gmail.com>
2017-07-08 21:18:01 -0400

    noop: scaffolding for easier main.go reading
    - moves cli parsing out of main.go
    - cleans up usage doc; implements --help flag parsing
    - implements arg-count errors
    - leaves errors for remaining cli-parsing
    - creates clean log.Logger for unified-looking CLI errors

---
+++ cli.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const usageDoc string = `sysrestic - an exclude-file joiner for system backups with restic
+
+Synopsis:
+  sysreestic [help] RESTIC_REPO EXCLUDE_FILE
+
+Description:
+  Execs to restic[1] to backup / to RESTIC_REPO path with an automatically
+  built list for restic's --exclude-file option.
+
+Outline:
+  1. visits every $HOME on the system
+  2. reads said $HOME's ~/.config/sysrestic.exclude or ~/.sysrestic.exclude
+  3. creates a new exclude-file, unifying all $HOME's excludes w/EXCLUDE_FILE
+  4. shells out to restic:
+       restic backup \
+          --repo RESTIC_REPO \
+          --exclude-file /path/to/temporary/unified/exclude-list \
+          /
+
+Reading Exclude Files:
+  For both system and users' exclude files, empty files are okay.
+
+  All lines in a user's exclude file are read as relative to their home.
+  Leading slashes are ignored. Not much care has been taken beyond this to
+  prevent bad things (eg: users may be able to exclude important files that do
+  not belong to them using hard-link walks, like "../../../").
+
+[1]: https://restic.github.io
+`
+
+func looksLikeHelp(arg string) bool {
+	return arg == "h" || arg == "-h" || arg == "--h" ||
+		arg == "help" || arg == "-help" || arg == "--help"
+}
+
+func parseCli(args []string) (*resticCmd, error) {
+	if len(args) != 2 {
+		if len(args) == 1 && looksLikeHelp(args[0]) {
+			fmt.Printf(usageDoc)
+			os.Exit(0)
+		}
+		return nil, fmt.Errorf("must provide 2 args, got %d", len(args))
+	}
+
+	r := &resticCmd{}
+	return nil, fmt.Errorf("parseCli(): ExcludeSysPath not yet parsed")
+	return nil, fmt.Errorf("parseCli(): ResticRepoPath not yet parsed")
+	return r, nil
+}
--- main.go
+++ main.go
@@ -5,52 +5,20 @@ import (
 	"os"
 )
 
-const usageDoc string = `
-  Name:
-    sysrestic - an exclude-file joiner for system backups with restic
-
-  Synopsis:
-    sysreestic RESTIC_REPO EXCLUDE_FILE
-
-  Description:
-    Execs to restic[1] to backup / to RESTIC_REPO path with an automatically
-    built list for restic's --exclude-file option.
-
-  Outline:
-    1. visits every $HOME on the system
-    2. reads said $HOME's ~/.config/sysrestic.exclude or ~/.sysrestic.exclude
-    3. creates a new exclude-file, unifying all $HOME's excludes w/EXCLUDE_FILE
-    4. shells out to restic:
-         restic backup \
-            --repo RESTIC_REPO \
-            --exclude-file /path/to/temporary/unified/exclude-list \
-            /
-
-  Reading Exclude Files:
-    For both system and users' exclude files, empty files are okay.
-
-    All lines in a user's exclude file are read as relative to their home.
-    Leading slashes are ignored. Not much care has been taken beyond this to
-    prevent bad things (eg: users may be able to exclude important files that do
-    not belong to them using hard-link walks, like "../../../").
-
-  [1]: https://restic.github.io
-`
-
 type resticCmd struct {
 	ExcludeSysPath string
 	ResticRepoPath string
 }
 
 func (c *resticCmd) parseExcludes() {
-	panic("parseExcludes() not yet implemented: exclude.ParseHomeConf() in loop, then exclude.Build()")
-}
-
-func parseCli(args []string) error {
-	panic("parseCli() not yet implemented")
+	log.Fatalf("parseExcludes() not yet implemented: exclude.ParseHomeConf() in loop, then exclude.Build()")
 }
 
 func main() {
-	log.Fatalf("not yet implemented\n")
-	parseCli(os.Args[1:])
+	lg := log.New(os.Stderr, "sysrestic: ", 0)
+	r, e := parseCli(os.Args[1:])
+	if e != nil {
+		lg.Fatalf("parsing command: %s\n", e)
+	}
+	lg.Fatalf("not yet implemented, but got: %s\n", r)
 }
