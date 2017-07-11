975d68153e48ed51b63500c3b99479caab11bd29
Jonathan Zacsh <jzacsh@gmail.com>
2017-07-10 20:38:32 -0400

    on-disk test strategy for file/write.go

--- file/write_test.go
+++ file/write_test.go
@@ -1,7 +1,25 @@
 package file
 
-import "testing"
+import (
+	"path/filepath"
+	"testing"
+
+	"../testdata"
+)
 
 func TestWriteAsciiLines(t *testing.T) {
-	t.Fatalf("WriteAsciiLines() tests not yet written")
+	testdata.AssertFixtureDir(t, fixtureDir)
+
+	p := filepath.Join(fixtureDir, "/non/existent/dir/not/possible/to/write/to/")
+	if WriteAsciiLines([]string{"foo"}, p) == nil {
+		t.Errorf("expected failure writing to: %s", p)
+	}
+
+	p = filepath.Join(fixtureDir, "/output/writetest.txt")
+	if e := WriteAsciiLines([]string{"foo", "bar", "baz"}, p); e != nil {
+		t.Errorf("unexpected failure writing to %s: %v", p, e)
+	}
+
+	// TODO diff against p against ex:
+	// ex = filepath.Join(fixtureDir, "/home/janet/pkgout/write.WriteAsciiLines_foobarbaz")
 }
