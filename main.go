jzacsh/sysrestic

commit d2458631bd76762a8c6a4b84bd96a2a3c155b191

fix #5: exclude self when backing up

main.go
@@ -45,7 +45,10 @@ func (c *resticCmd) parseExcludes() error {
 		c.UserExcludes++
 	}
 
-	unified, e := exclude.Build(c.ExcludeSysPath, excs...)
+	unified, e := exclude.Build(
+		c.ExcludeSysPath,
+		[]string{c.ResticRepoPath}, // ensure restic excludes its own repo
+		excs...)
 	if e != nil {
 		return e
 	}

commit 3412712c15c8525dbe75981a572cf62ebdf2111d

fix build break caused by d2458631bd767

main.go
@@ -35,7 +35,9 @@ func (c *resticCmd) parseExcludes() error {
 	}
 	c.Users = len(homes)
 
-	var excs [][]string
+	// ensure restic excludes its own repo
+	excs := [][]string{[]string{c.ResticRepoPath}}
+
 	for _, home := range homes {
 		excludes, e := exclude.ParseHomeConf(home)
 		if e != nil {
@@ -45,10 +47,7 @@ func (c *resticCmd) parseExcludes() error {
 		c.UserExcludes++
 	}
 
-	unified, e := exclude.Build(
-		c.ExcludeSysPath,
-		[]string{c.ResticRepoPath}, // ensure restic excludes its own repo
-		excs...)
+	unified, e := exclude.Build(c.ExcludeSysPath, excs...)
 	if e != nil {
 		return e
 	}